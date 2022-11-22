c14baff4ad636e71e70226f4d5ff329a33ca89a9
Sleepingisimportant <43533116+Sleepingisimportant@users.noreply.github.com>
2022-11-22 17:43:37 +0100

    add api/attraction/<id> api/attractions

--- app.py
+++ app.py
@@ -1,20 +1,191 @@
 from flask import *
-app=Flask(__name__)
-app.config["JSON_AS_ASCII"]=False
-app.config["TEMPLATES_AUTO_RELOAD"]=True
 
-# Pages
+import mysql.connector
+from mysql.connector import errorcode
+from werkzeug.exceptions import HTTPException
+
+
+app = Flask(__name__,
+            static_folder="public",
+            static_url_path="/")
+
+app.config['JSON_AS_ASCII'] = False
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+app.secret_key = "any string but secret"
+
+
+#### create connection pool ####
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'mysqlpw1!',
+    'database': 'website',
+    'port': 3306,
+}
+cnxpool = mysql.connector.pooling.MySQLConnectionPool(
+    pool_name='website_dbp', pool_size=20, pool_reset_session=True, **db_config)
+
+try:
+    cnx = cnxpool.get_connection()
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+
+cur = cnx.cursor(buffered=True)
+
+
+
+# handle error
+def error_messsage(message):
+    return {
+        "error": True,
+        "message": message
+    }
+
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    # pass through HTTP errors
+    if isinstance(e, HTTPException):
+        return e
+
+    # now you're handling non-HTTP exceptions only
+    return error_messsage("Internal Server Error."), 500
+
+
+
+
+
+@app.route("/api/attractions")
+def attraction():
+
+
+    keyword = request.args.get("keyword")
+    page = request.args.get("page")
+
+    if page is None or page == "":
+        err = "Please specify the page number."
+        return error_messsage(err)
+    else:
+        try:
+            pageInt = int(page)
+        except ValueError:
+            err = "Page value is valid."
+            return error_messsage(err)
+
+    if pageInt < 0:
+        err = "Page number is not valid."
+        return error_messsage(err)
+
+    else:
+        rowFrom = pageInt*12
+        rowTo = (pageInt+1)*12
+
+        if keyword is None:
+            cur.execute(
+                "SELECT * FROM attraction limit %s, %s ", (rowFrom, rowTo))
+        else:
+            keyword = "%"+keyword+"%"
+            print(keyword)
+            cur.execute("""SELECT * FROM attraction WHERE name LIKE %s OR CAT LIKE %s OR description LIKE %s OR address LIKE %s OR direction LIKE %s OR MRT LIKE %s limit %s, %s """,
+                        (keyword, keyword, keyword, keyword, keyword, keyword, rowFrom, rowTo,))
+
+        data = cur.fetchall()
+
+        nextPage = None if len(data) < 12 else (pageInt+1)
+
+        json = {
+            "nextPage": nextPage,
+            "data": [
+            ]}
+
+        for d in data:
+
+            # clear non-image file
+            imageFile = d[15]
+            fileSplit = imageFile.split("https")
+            https = "https"
+            imagesList = [https+x for x in fileSplit[1:]]
+
+            attraction = {
+                "id": d[0],
+                "name": d[3],
+                "category": d[12],
+                "description": d[18],
+                "address": d[20],
+                "transport": d[2],
+                "mrt": d[9],
+                "lat": d[17],
+                "lng": d[5],
+                "images": imagesList
+            }
+            json["data"].append(attraction)
+
+        return json
+
+
+@app.route("/api/attraction/<id>")
+def attraction_with_id(id):
+    print("1111111")
+
+    cur.execute("SELECT * FROM attraction WHERE ID= %s", (id,))
+    data = cur.fetchone()
+
+ 
+    json = {
+        "data": [
+        ]}
+
+    # clear non-image file
+    imageFile = data[15]
+    fileSplit = imageFile.split("https")
+    https = "https"
+    imagesList = [https+x for x in fileSplit[1:]]
+
+    attraction = {
+        "id": data[0],
+        "name": data[3],
+        "category": data[12],
+        "description": data[18],
+        "address": data[20],
+        "transport": data[2],
+        "mrt": data[9],
+        "lat": data[17],
+        "lng": data[5],
+        "images": imagesList
+    }
+    json["data"].append(attraction)
+    print(data)
+
+    return json
+
+
+@app.route("/api/categories")
+def categories():
+
+    cur.execute("SELECT DISTINCT CAT FROM attraction")
+    data = cur.fetchall()
+
+
+ 
+    json = {
+        "data":  [d[0] for d in data]
+        }
+    return json
+
+
 @app.route("/")
 def index():
-	return render_template("index.html")
-@app.route("/attraction/<id>")
-def attraction(id):
-	return render_template("attraction.html")
-@app.route("/booking")
-def booking():
-	return render_template("booking.html")
-@app.route("/thankyou")
-def thankyou():
-	return render_template("thankyou.html")
+    return render_template("index.html")
 
-app.run(port=埠號)
+
+
+
+if __name__ == '__main__':
+    app.run(port=3000)
---
+++ data/data_to_database.py
@@ -0,0 +1,87 @@
+from flask import Flask
+import json
+
+import mysql.connector
+import MySQLdb
+from mysql.connector import errorcode
+
+
+app = Flask(__name__,
+            static_folder="public",
+            static_url_path="/")
+
+app.config['JSON_AS_ASCII'] = False
+
+app.secret_key = "any string but secret"
+
+
+#### create connection pool ####
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'mysqlpw1!',
+    'database': 'website',
+    'port': 3306,
+}
+cnxpool = mysql.connector.pooling.MySQLConnectionPool(
+    pool_name='website_dbp', pool_size=20, pool_reset_session=True, **db_config)
+
+try:
+    cnx = cnxpool.get_connection()
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+
+cur = cnx.cursor(buffered=True)
+
+file_path = "data/taipei-attractions.json"
+f = open(file_path, 'r')
+
+json_load_data = json.load(f)
+data = json_load_data["result"]["results"]
+f.close()
+
+
+for d in data:
+    rate = d["rate"]
+    direction = d["direction"]
+    name = d["name"]
+    date = d["date"]
+    longitude = d["longitude"]
+    ref_wp = d["REF_WP"]
+    avBegin = d["avBegin"]
+    langinfo = d["langinfo"]
+    mrt = d["MRT"]
+    serialNo = d["SERIAL_NO"]
+    RowNumber = d["RowNumber"]
+    cat = d["CAT"]
+    memoTime = d["MEMO_TIME"]
+    poi = d["POI"]
+    idpt = d["idpt"]
+    latitude = d["latitude"]
+    description = d["description"]
+    idNum = d["_id"]
+    avEnd = d["avEnd"]
+    address = d["address"]
+
+    file = d["file"]
+    fileSplit = file.split("https")
+    jpg = ".jpg"
+    png = ".png"
+    http = "http"
+    file_cleaned = ''.join(
+        map(str, [http+x for x in fileSplit if jpg in x.lower() or png in x.lower()]))
+
+    insert_attraction = ("INSERT INTO attraction "
+                         "(rate, direction, name, date, longitude, REF_WP, avBegin, langinfo, MRT, SERIAL_NO, RowNumber, CAT, MEMO_TIME, POI, file, idpt, latitude, description, ID, avEnd, address)"
+                         "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
+
+    cur.execute(insert_attraction, (rate, direction, name, date, longitude, ref_wp, avBegin, langinfo, mrt,
+                                    serialNo, RowNumber, cat, memoTime, poi, file_cleaned, idpt, latitude, description, idNum, avEnd, address))
+
+cnx.commit()
